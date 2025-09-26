47ed2720db49a59f0c8c91a656d244aefe51ce13
Kai Hsieh <kaihsieh@cayintech.com>
2025-09-26 15:04:25 +0800

    fix

--- build_all.sh
+++ build_all.sh
@@ -2,7 +2,7 @@
 
 git submodule update --init --recursive
 
-curl -s https://raw.githubusercontent.com/shadowkai0121/zh_tw_correct/refs/heads/master/correct.py | py 'https://raw.githubusercontent.com/shadowkai0121/zh_tw_correct/refs/heads/master/DICTIONARY' -c correct.txt
+curl -s https://raw.githubusercontent.com/shadowkai0121/zh_tw_correct/refs/heads/master/correct.py | py - 'https://raw.githubusercontent.com/shadowkai0121/zh_tw_correct/refs/heads/master/DICTIONARY' -c correct.txt
 
 IFS=$'\n'
 find . -type f -name "book.yml" -print0 | while IFS= read -r -d '' file; do
